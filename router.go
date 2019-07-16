6c57f8c7a23c98bcfea99b23c5ef940d9a8509de
hendraronaldi <hendraronaldi@hotmail.com>
2019-07-16 21:24:35 +0700

    change edituser post to put

--- router.go
+++ router.go
@@ -38,7 +38,7 @@ func Router(router *gin.Engine) {
 }
 
 func UsersRouter(router *gin.Engine) {
-	router.POST("/edituser", controller.EditUser)
+	router.PUT("/edituser", controller.EditUser)
 	router.POST("/register", controller.Register)
 	router.POST("/login", controller.Login)
 	// router.POST("/welcome", controller.Welcome)
