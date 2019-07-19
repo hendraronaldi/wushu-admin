0a6e377a89ccd83e1179547fa1fdb74d58e3c09e
hendraronaldi <hendraronaldi@hotmail.com>
2019-07-19 21:50:59 +0700

    change user data

--- modules/model/user.go
+++ modules/model/user.go
@@ -1,21 +1,20 @@
 package model
 
 type User struct {
-	Name            string `json:"name"`
-	Username        string `json:"username"`
-	Email           string `json:"email"`
-	Password        string `json:"password"`
-	Birthdate       string `json:"birthdate"`
-	Birthplace      string `json:"birthplace"`
-	AddressNow      string `json:"addressnow"`
-	AddressHometown string `json:"addresshometown"`
-	Phone           string `json:"phone"`
-	PhoneHome       string `json:"phonehome"`
-	NameParent      string `json:"nameparent"`
-	PhoneParent     string `json:"phoneparent"`
-	ExpPrev         string `json:"expprev"`
-	Motivation      string `json:"motivation"`
-	Status          int    `json:"status"`
+	Name          string `json:"name"`
+	Username      string `json:"username"`
+	Email         string `json:"email"`
+	Password      string `json:"password"`
+	Birthdate     string `json:"birthdate"`
+	Birthplace    string `json:"birthplace"`
+	Address       string `json:"address"`
+	AddressParent string `json:"addressparent"`
+	Phone         string `json:"phone"`
+	NameParent    string `json:"nameparent"`
+	PhoneParent   string `json:"phoneparent"`
+	ExpPrev       string `json:"expprev"`
+	Motivation    string `json:"motivation"`
+	Status        int    `json:"status"`
 }
 
 type Login struct {
