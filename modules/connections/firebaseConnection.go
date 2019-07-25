2fa1fad48625209b288e32ba04324ce1c182ef0b
hendraronaldi <hendraronaldi@hotmail.com>
2019-07-25 17:51:03 +0700

    course tutorial db connection

--- modules/connections/firebaseConnection.go
+++ modules/connections/firebaseConnection.go
@@ -39,3 +39,31 @@ func FirebaseConnection() *firestore.Client {
 
 	return client
 }
+
+func FirebaseTutorialConnection() *firestore.Client {
+	ctx := context.Background()
+	var err error
+	var configJSON []byte
+
+	if configJSONString := os.Getenv("firebase-tutorial-auth"); configJSONString != "" {
+		configJSON = []byte(configJSONString)
+	} else {
+		configJSON, err = ioutil.ReadFile("utilities/wushu-tutorial-firebase.json")
+		if err != nil {
+			fmt.Println("read file err", err)
+			return nil
+		}
+	}
+	sa := option.WithCredentialsJSON(configJSON)
+	app, err := firebase.NewApp(ctx, nil, sa)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	client, err := app.Firestore(ctx)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	return client
+}
