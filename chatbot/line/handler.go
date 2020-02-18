87063a893c11a114ef0f620facd21c951ef3d9a6
hendraronaldi <hendraronaldi@hotmail.com>
2020-02-18 22:46:18 +0700

    fix case sensitive user

--- chatbot/line/handler.go
+++ chatbot/line/handler.go
@@ -44,22 +44,34 @@ func ReplyHandler(id string, m linebot.Message) []linebot.Message {
 					botReply = append(botReply, messages.TextMessage("Your name is: "+riveReply))
 					botReply = append(botReply, messages.ConfirmCustomMessage("Are you sure?", confirmation))
 				} else if strings.HasPrefix(strings.ToLower(message.Text), "yes\n") {
-					fmt.Println("sending registration confirmation")
-					var confirmation []map[string]string
-					yes := make(map[string]string)
-					no := make(map[string]string)
+					var newUser model.LineUser
+					confirmationDetails := strings.Split(message.Text, "\n")
 
-					yes["Yes"] = strings.ToLower(message.Text)
-					no["No"] = "no\n" + strings.ToLower(message.Text)[4:]
-					confirmation = append(confirmation, yes, no)
+					newUser.ID = confirmationDetails[2]
+					newUser.Name = confirmationDetails[3]
+					newUser.Status = false
 
-					botPushMessage = append(botPushMessage, messages.TextMessage(strings.ToLower(message.Text)))
-					botPushMessage = append(botPushMessage, messages.ConfirmCustomMessage("Are you sure?", confirmation))
-					err := PushHandler(adminID, botPushMessage)
+					err := controller.AddLineUser(newUser)
 					if err != nil {
 						botReply = append(botReply, messages.TextMessage("Fail to send user registration"))
 					} else {
-						botReply = append(botReply, messages.TextMessage("User registration has been sent, please wait for the confirmation"))
+						fmt.Println("sending registration confirmation")
+						var confirmation []map[string]string
+						yes := make(map[string]string)
+						no := make(map[string]string)
+
+						yes["Yes"] = message.Text
+						no["No"] = "no\n" + message.Text[4:]
+						confirmation = append(confirmation, yes, no)
+
+						botPushMessage = append(botPushMessage, messages.TextMessage(message.Text))
+						botPushMessage = append(botPushMessage, messages.ConfirmCustomMessage("Are you sure?", confirmation))
+						err := PushHandler(adminID, botPushMessage)
+						if err != nil {
+							botReply = append(botReply, messages.TextMessage("Fail to send user registration"))
+						} else {
+							botReply = append(botReply, messages.TextMessage("User registration has been sent, please wait for the confirmation"))
+						}
 					}
 				} else {
 					fmt.Println("new user welcome")
